9f78b11c4451d74ca7ed2bebcd9c27b9dc2de508
Somanos Sar <somanos@drumee.net>
2024-09-05 17:18:44 +0200

    Changed prepare integration by debian package builder

--- .dev-tools.rc/deploy.sh
+++ .dev-tools.rc/deploy.sh
@@ -1,7 +1,1 @@
-# to deploy code on remote server, copy this file to .env/drumee.sh, then
-# uncomment lines below and set proper values according to your infra configs
-# Finally, run npm run dev
-
-export DEST_HOST=drumee.io
-export DEST_DIR=/srv/drumee/runtime/server/plugins/main/sandbox-server
-#export SRC_PATH=$HOME/github/sandbox-server
+export DEST_DIR=$HOME/debian/drumee-common/var/lib/drumee/setup
--- .dev-tools.rc/devel.sh
+++ .dev-tools.rc/devel.sh
@@ -1,8 +1,2 @@
-# to deploy code on remote server, copy this file to .env/drumee.sh, then
-# uncomment lines below and set proper values according to your infra configs
-# Finally, run npm run dev
-
-#export DEST_HOST=drumee.io
-#export DEST_USER=
 export DEST_DIR=$HOME/debian/drumee-common/var/lib/drumee/setup
 
